8a541c50f2d175c219c432ceb48a9a1340d1b5dd
Jordan Richmeier <jordan.richmeier2@gmail.com>
2023-03-28 17:31:42 -0600

    Refactored dev-setup scripts

--- docker/dev/dev-setup.sh
+++ docker/dev/dev-setup.sh
@@ -1,34 +1,44 @@
+oli_npm_install() {
+  echo "Installing node packages for \$0..."
+  cd $1
+  npm install
+  echo "Setting up pre-commit hooks..."
+  npx husky install
+  npx husky add .husky/pre-commit "exec >/dev/tty 2>&1"
+  npx husky add .husky/pre-commit "npx lint-staged"
+  echo "Installation complete!\n\n"
+  cd $2
+}
+
 # OLI Dev Environment Setup
 
-echo "Running initial set up of the OLI development environment. This will take several minutes."
+echo "Running initial set up of the OLI development environment"
+echo "This process will take several minutes to complete, please be patient"
+echo "\n\n"
 
 # Pull repository submodules
 echo "Pulling Repository Sub-Modules..."
 cd ../../
 git pull-all
-echo "Pull successful!"
+echo "Pull successful!\n\n"
 
 # Npm Installs
-echo "Installing backend node packages..."
-cd ./backend/ai-assist
-npm install
-cd ../core
-npm install
-cd ../inspections
-npm install
+echo "------------ Staring npm package installation ------------\n\n"
 
-cd ../../frontend/marketing
-npm install
-cd ../platform
-npm install
+oli_npm_install backend-ai-assist ./backend/ai-assist ../../
+oli_npm_install backend-core ./backend/core ../../
+oli_npm_install backend-inspections ./backend/inspections ../../
 
-cd ../../mobile/home-owner
-npm install
-cd ../inspector
-npm install
-echo "Node Packages install successful"
+oli_npm_install frontend-marketing ./frontend/marketing ../../
+oli_npm_install frontend-platform ./frontend/platform ../../
+
+oli_npm_install mobile-inspector-app ./mobile/inspector ../../
+oli_npm_install mobile-home-owner-app ./mobile/home-owner ../../
+
+echo "\nNode package installation successful!\n\n"
 
 # Docker Container Builds
-echo "Building docker containers..."
+echo "Starting Docker Container build process..."
+
 cd ../../docker/dev
 docker-compose up --build
